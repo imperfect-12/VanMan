41848732b5efd067da09ad020bbc2758c798fdce
Prashant Bisht <bishtprashant121@gmail.com>
2026-05-31 13:18:18 +0530

    removed the env details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,6 @@ import Footer from "./components/Footer";
 import Login from "./pages/Login.jsx";
 import { useAuthContext } from "./contexts/AuthContext.jsx";
 
-// MONGODB_URI=mongodb+srv://bishtprashant:NgE4NXmrfZcb7imz@cluster0.hfaznri.mongodb.net/
-// JWT_SECRET=c3deed6ccd1bee6f22ea401fbe13d99d34272516b44833bd147e36e5f330533ccd
-// NODE_ENV=development
-// PORT=3000
-
 function App() {
   const { isAuthenticated } = useAuthContext();
   return (
